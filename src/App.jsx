46afeae7d59dc0a50d31168d569fc58514cc4c19
Daksh-Badhwar <badhwardakshhwhlpj369@gmail.com>
2025-05-15 15:49:53 +0530

    Hard coded components

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./App.css";
+
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItem1 from "./components/TodoItem1";
+import TodoItem2 from "./components/TodoItem2";
+function App() {
+  return (
+    <center className="todo-container">
+      <AppName></AppName>
+      
+      <div className="items-container">
+        
+        <AddTodo></AddTodo>
+      <TodoItem1/>
+      <TodoItem2/>
+      </div>
+
+    </center>
+
+  )}
+
+export default App
---
+++ src/components/AppName.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const AppName=()=>{
+return <h1> Todo React app </h1>;
+
+    
+
+}
+export default AppName;
