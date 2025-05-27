5660b8ff3d00c5bdc00828cc6b905caa04ce12bb
Daksh-Badhwar <badhwardakshhwhlpj369@gmail.com>
2025-05-27 21:05:24 +0530

    UseReducer hook 1

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import TodoItem from "./components/TodoItem";
 import TodoItems from "./components/TodoItems";
-import React,{useState} from 'react';
+import React,{useReducer} from 'react';
 import WelcomeMessage from "./components/WelcomeMessage";
 import {TodoItemsContext} from "./store/todo-items-store";
 
@@ -13,23 +13,64 @@ import {TodoItemsContext} from "./store/todo-items-store";
 
 //list is with app so for delete we need list
 
+
+const todoItemsReducer=(prevState,action)=>{
+  let newTodoItems= prevState;
+
+  if(action.type==="NEW_ITEM")
+  {
+     newTodoItems=
+     [...prevState,{name:action.payload.itemName,dueDate:action.payload.itemDueDate}]
+  }
+  else if (action.type==="DELETE_ITEM")
+  {
+  {
+  newTodoItems= prevState.filter((item)=>item.name!==action.payload.itemName);
+ 
+}
+  }
+
+  return newTodoItems;
+}
+
+
 function App() {
  
 
- 
 
-const [todoItems,setTodoItems]=useState([]);
+
+//const [todoItems,setTodoItems]=useState([]);
+ const[todoItems,dispatchTodoItems]=useReducer(todoItemsReducer,[]);
+
+
 const handleNewItem = (itemName,itemDueDate)=>{
 
-setTodoItems((prevState)=>[...prevState,{name:itemName,dueDate:itemDueDate}]);
+const newItemAction = {
+
+  type:"NEW_ITEM",
+  payload:{
+      itemName: itemName,
+       itemDueDate: itemDueDate,}
+
+};
+dispatchTodoItems(newItemAction);
+
+
+//setTodoItems((prevState)=>[...prevState,{name:itemName,dueDate:itemDueDate}]);
 }
 
 
 const handleDeleteItem= (toDoItemName)=>{
- const newtodoItems= todoItems.filter((item)=>item.name!==toDoItemName);
- setTodoItems(newtodoItems);
-}
 
+const deleteItemAction = {
+
+  type:"DELETE_ITEM",
+  payload:{
+      itemName: toDoItemName,
+       }
+
+};
+dispatchTodoItems(deleteItemAction);}
 
   return (
     <TodoItemsContext.Provider value={{
